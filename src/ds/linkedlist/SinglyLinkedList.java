97a160ff03f7763a79810e79994e55507f081bb8
Chaminda Kumara <Chaminda.Kumara2@dialog.lk>
2021-05-10 01:18:48 +0530

    method signature

--- src/ds/linkedlist/SinglyLinkedList.java
+++ src/ds/linkedlist/SinglyLinkedList.java
@@ -1,4 +1,47 @@
 package ds.linkedlist;
 
 public class SinglyLinkedList {
+
+    private Node headNode;
+
+    public void add(int number) {
+
+    }
+
+    public void add(int index, int number) {
+
+    }
+
+    public void remove(int index) {
+
+    }
+
+    public int get(int index) {
+
+        return -1;
+    }
+
+    public void print() {
+
+    }
+
+    public void clear() {
+
+    }
+
+    public int size() {
+
+        return -1;
+    }
+
+    public boolean contains(int number) {
+
+        return false;
+    }
+
+    public boolean empty() {
+        return false;
+    }
+
 }
+
