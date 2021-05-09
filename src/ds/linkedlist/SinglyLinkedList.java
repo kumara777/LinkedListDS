a7a79a76c25b1b0216985b95b643806642877e4f
Chaminda Kumara <Chaminda.Kumara2@dialog.lk>
2021-05-10 02:03:50 +0530

    add(), remove() impl

--- src/ds/linkedlist/SinglyLinkedList.java
+++ src/ds/linkedlist/SinglyLinkedList.java
@@ -5,6 +5,20 @@ public class SinglyLinkedList {
     private Node headNode;
 
     public void add(int number) {
+        if (size() == 0) {
+            headNode = new Node();
+            headNode.number = number;
+            headNode.nextNode = null;
+        } else {
+            Node current =  new Node();
+            current = headNode;
+            if (current.nextNode == null) {
+                Node temp = new Node();
+                temp.number = number;
+                temp.nextNode = null;
+                current.nextNode = temp;
+            }
+        }
 
     }
 
@@ -13,11 +27,47 @@ public class SinglyLinkedList {
     }
 
     public void remove(int index) {
-
+        if (index == 0) {
+            headNode = null;
+            return;
+        }
+        int incrementNode = 0;
+        Node current =  new Node();
+        current = headNode;
+        while (current != null) {
+            if (incrementNode == index) {
+                Node temp = new Node();
+                temp = headNode;
+                for (int i = 0; i < incrementNode - 1; i++) {
+                    temp = temp.nextNode;
+                }
+                temp.nextNode = current.nextNode;
+                headNode = temp;
+                break;
+            } else {
+                current = current.nextNode;
+                incrementNode++;
+            }
+        }
     }
 
     public int get(int index) {
-
+        if (empty()) {
+            throw new RuntimeException("Singly Linked List is empty");
+        }
+        if (size() <= index) {
+            throw new RuntimeException("Index is out of current Singly Linked List");
+        }
+        Node current =  new Node();
+        int incrementNode = 0;
+        current = headNode;
+        while (current != null) {
+            if (incrementNode == index) {
+                return current.number;
+            }
+            incrementNode++;
+            current = current.nextNode;
+        }
         return -1;
     }
 
--- src/ds/linkedlist/SinglyLinkedListTest.java
+++ src/ds/linkedlist/SinglyLinkedListTest.java
@@ -4,11 +4,18 @@ public class SinglyLinkedListTest {
 
     public static void main(String[] args) {
         SinglyLinkedList sLL = new SinglyLinkedList();
-        System.out.println(sLL.empty()); //1
-        System.out.println(sLL.contains(12)); //2
+        System.out.println(sLL.empty()); //1   ==> true
+        System.out.println(sLL.contains(12)); //2 ==> false
         sLL.clear(); //3
-        System.out.println(sLL.size()); //4
-        sLL.print(); //5
+        System.out.println(sLL.size()); //4 ==> 0
+        sLL.print(); //5 ==> []
+     //   sLL.get(4); //6 java.lang.RuntimeException: Singly Linked List is empty
+        sLL.remove(0);//7
+        sLL.add(5); //8
+        System.out.println(sLL.size()); //9  ==> 1
+        sLL.add(23); //10 ==>
+        sLL.print(); //11 ==> [5, 23]
+        System.out.println(sLL.size()); //12  ==> 2
     }
 
 }
